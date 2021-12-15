840a5cefc6f046c528641fdf8ed75e7fb27c815f
Dhee-dishant <96163071+Dhee-dishant@users.noreply.github.com>
2021-12-15 11:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,153 @@
-import streamlit as st
-import sounddevice as sd
-import wavio
-import threading 
 import logging
+import logging.handlers
+import queue
+import threading
+import time
+import urllib.request
+from collections import deque
+from pathlib import Path
+from typing import List
+
+import av
+import numpy as np
+import pydub
+import streamlit as st
+
+from streamlit_webrtc import (
+    AudioProcessorBase,
+    ClientSettings,
+    WebRtcMode,
+    webrtc_streamer,
+)
+
+HERE = Path(__file__).parent
+
+logger = logging.getLogger(__name__)
 
 
-class StreamLitApp:
-  def __init__(self):
-    self.SAMPLE_RATE = 44100
-    self.mic_queue = []
-    self.CHANNELS = 3
-    self.x = threading.Thread(target=self.record)
-    self.boolean = True
+# This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
+def download_file(url, download_to: Path, expected_size=None):
+    # Don't download the file twice.
+    # (If possible, verify the download using the file length.)
+    if download_to.exists():
+        if expected_size:
+            if download_to.stat().st_size == expected_size:
+                return
+        else:
+            st.info(f"{url} is already downloaded.")
+            if not st.button("Download again?"):
+                return
 
+    download_to.parent.mkdir(parents=True, exist_ok=True)
 
-  def callback(self, indata, frames, time, status):
-      if status:
-          print(status, file=sys.stderr)
-      logging.info(indata.copy())
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % url)
+        progress_bar = st.progress(0)
+        with open(download_to, "wb") as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
 
-  def record(self):
-    with sd.InputStream(samplerate=self.SAMPLE_RATE, channels=self.CHANNELS, callback=self.callback):
-        while True:
-          if self.boolean:
-            break
-        
-  def stop_mic_recording(self):
-    self.boolean = True
-    print("Stop button")
-    self.x.join()
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning(
+                        "Downloading %s... (%6.2f/%6.2f MB)"
+                        % (url, counter / MEGABYTES, length / MEGABYTES)
+                    )
+                    progress_bar.progress(min(counter / length, 1.0))
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
 
-  def Record_Btn(self):
-    self.boolean = False
-    self.x.start()
 
 def main():
-  s = StreamLitApp()
-  if st.button(f"Click to Record"):
-    s.Record_Btn()
-  if st.button(f"Click to Stop"):
-    s.stop_mic_recording()
+    st.header("Real Time Speech-to-Text")
 
-if __name__ == '__main__':
-  main()
+    sound_only_page = "Sound only (sendonly)"
+    app_mode = sound_only_page
+
+    if app_mode == sound_only_page:
+        app_sst()
+
+def app_sst():
+    webrtc_ctx = webrtc_streamer(
+        key="speech-to-text",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=1024,
+        client_settings=ClientSettings(
+            rtc_configuration={
+                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            },
+            media_stream_constraints={"video": False, "audio": True},
+        ),
+    )
+
+    status_indicator = st.empty()
+
+    if not webrtc_ctx.state.playing:
+        return
+
+    status_indicator.write("Loading...")
+    text_output = st.empty()
+    stream = None
+
+    while True:
+        if webrtc_ctx.audio_receiver:
+
+            sound_chunk = pydub.AudioSegment.empty()
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                time.sleep(0.1)
+                status_indicator.write("No frame arrived.")
+                continue
+
+
+            status_indicator.write("Running. Say something!")
+
+            for audio_frame in audio_frames:
+                sound = pydub.AudioSegment(
+                    data=audio_frame.to_ndarray().tobytes(),
+                    sample_width=audio_frame.format.bytes,
+                    frame_rate=audio_frame.sample_rate,
+                    channels=len(audio_frame.layout.channels),
+                )
+                sound_chunk += sound
+                logger.info(sound)
+
+        else:
+            status_indicator.write("AudioReciver is not set. Abort.")
+            break
+
+
+if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
